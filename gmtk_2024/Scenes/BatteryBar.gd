6d8c17eca32baeab24e3744a8c668d5cd862b005
Christian Gonzales <christian.gonzales@digipen.edu>
2024-08-16 17:10:04 -0700

    battery progress bar counts down

--- gmtk_2024/Scenes/BatteryBar.gd
+++ gmtk_2024/Scenes/BatteryBar.gd
@@ -1,4 +1,4 @@
-extends ProgressBar
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
@@ -7,5 +7,11 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#func _process(delta: float) -> void:
+	#pass
+	
+	
+
+
+func _on_timer_timeout() -> void:
+	$ProgressBar.value -= 0.01
