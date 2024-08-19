1ca2a7b9cdc527b62662c32ecf847c5957e4cd68
Christian Gonzales <christian.gonzales@digipen.edu>
2024-08-19 00:39:33 -0700

    button fixed
    The Quit button Quits the game while the play button start the game

--- gmtk_2024/main_menu.gd
+++ gmtk_2024/main_menu.gd
@@ -1,8 +1,9 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
+
 	pass # Replace with function body.
 
 
@@ -10,13 +11,10 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func _on_game_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/joshua_test_scene.tscn")
+
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
-	pass # Replace with function body.
-
-
-func _on_game_button_pressed() -> void:
-	$Transition.go = true
-	$Transition.scene = "res://Scenes/joshua_test_scene.tscn"
 	pass # Replace with function body.
